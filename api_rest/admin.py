1159a905cec48c116ab3ef6cb9bdf9272ec804f3
Jaidiber <jaider.9507@gmail.com>
2019-07-28 22:54:29 -0500

    migrate db and add method __str__ to all class model

--- api_rest/admin.py
+++ api_rest/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, SubCategory, Aspect, Problem, School, Teacher, Group, Student, StudentXGroup, Exam, Test, StudentAnswer
+### mathmeter
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Aspect)
+admin.site.register(Problem)
+admin.site.register(School)
+admin.site.register(Teacher)
+admin.site.register(Group)
+admin.site.register(Student)
+admin.site.register(StudentXGroup)
+admin.site.register(Exam)
+admin.site.register(Test)
+admin.site.register(StudentAnswer)
--- api_rest/models.py
+++ api_rest/models.py
@@ -4,55 +4,79 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.name
 
 class SubCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     category = models.ForeignKey('Category', on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Aspect(models.Model):
     objective = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     sub_cat = models.ForeignKey('SubCategory', on_delete = models.CASCADE)
+    def __str__(self):
+        return self.objective
 
 class Problem(models.Model):
     question = models.TextField()
     answer = models.FloatField()
     aspect = models.ForeignKey('Aspect', on_delete = models.CASCADE)
+    def __str__(self):
+        return self.question
 
 class School(models.Model):
     name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 class Teacher(models.Model):
     name = models.CharField(max_length=200)
     id_number = models.CharField(max_length=20)
     birth_date = models.DateField(null = True, blank = True)
     school = models.ForeignKey('School', on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Group(models.Model):
     name = models.CharField(max_length=200)
     teacher = models.ForeignKey('Teacher', on_delete = models.SET_NULL, null = True, blank = True)
     school = models.ForeignKey('School', on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Student(models.Model):
      name = models.CharField(max_length=200)
+     def __str__(self):
+        return self.name
 
 class StudentXGroup(models.Model):
      id_school = models.CharField(max_length=200)
      group = models.ForeignKey('Group', on_delete = models.CASCADE)
      student = models.ForeignKey('Student', on_delete = models.CASCADE)
+     def __str__(self):
+        return self.id_school
 
 class Exam(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     teacher = models.ForeignKey('Teacher', on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Test(models.Model):
     exam = models.ForeignKey('Exam', on_delete = models.CASCADE)
     aspect = models.ForeignKey('Aspect', on_delete = models.CASCADE)
     weight = models.FloatField()
+    def __str__(self):
+        return str(self.weight)
 
 class StudentAnswer(models.Model):
     test = models.ForeignKey('Test', on_delete = models.CASCADE)
     sxg = models.ForeignKey('StudentXGroup', on_delete = models.CASCADE)
     failed = models.BooleanField(default = True)
+    def __str__(self):
+        return str(self.test) +" "+ str(self.sxg)
